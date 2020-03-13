77bfb33cab71f61812b28a2d79afd3003b0b393e
Nick Balnaves <nick@192-168-1-129.tpgi.com.au>
2020-03-13 13:36:30 +1100

    Create scaffold methods

--- lib/dual_screen.dart
+++ lib/dual_screen.dart
@@ -1,22 +1,4 @@
-import 'dart:async';
+library dual_screen;
 
-import 'package:flutter/services.dart';
-
-class DualScreen {
-  static const MethodChannel _methodChannel =
-      const MethodChannel('plugins.builttoroam.com/dual_screen/methods');
-  static const EventChannel _eventChannel =
-      const EventChannel('plugins.builttoroam.com/dual_screen/events');
-
-  static Future<bool> get isDualScreenDevice async =>
-      await _methodChannel.invokeMethod('isDualScreenDevice');
-
-  static Future<bool> get isAppSpanned async =>
-      await _methodChannel.invokeMethod('isAppSpanned');
-
-  static Stream<bool> isAppSpannedStream() async* {
-    yield* _eventChannel
-        .receiveBroadcastStream()
-        .map<bool>((dynamic result) => result);
-  }
-}
+export 'dual_screen_methods.dart';
+export 'dual_screen_scaffold.dart';
